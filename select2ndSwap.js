1495bede2548dcb2fe0e50eeab9a1f44050f91fb
hacksh4w <mrinalsnair@gmail.com>
2023-07-17 19:51:33 +0530

    feat : clicks 2nd swape option

--- select2ndSwap.js
+++ select2ndSwap.js
@@ -18,12 +18,25 @@
 async function select2ndSwap(page, divSelector) { //sc-bb167634-2 gyEpxF
     // Wait for the second RouteWrapper element to load within the div 
     //await page.waitForSelector(`${divSelector}:nth-child(1)`, { visible: true });
-    await page.waitForSelector(`${divSelector} .RouteWrapper`, { visible: true });
+    //await page.waitForSelector(`${divSelector} .RouteWrapper`, { visible: true });
   
     // Click on the second RouteWrapper element
-    await page.click(`${divSelector} .RouteWrapper`);
-  
-    console.log('Clicked on the second RouteWrapper element.');
+    //await page.click(`${divSelector} .RouteWrapper`);
+{/*await page.$eval(`${divSelector} > .RouteWrapper`, element => element.click());
+    
+  // Click on the second div element
+  await page.$eval(`${divSelector} > div.sc-d413ea6e-0`, div => div.click());
+
+console.log('Clicked on the second RouteWrapper element.'); */}
+const divs = await page.$$(`div.sc-d413ea6e-0`);
+if (divs.length >= 2) {
+  const secondDiv = divs[1];
+  await secondDiv.click();
+  console.log('Clicked on the second div element.');
+} else {
+  console.log('Second div element not found.');
+}
+
 
 }
   module.exports = { select2ndSwap };
